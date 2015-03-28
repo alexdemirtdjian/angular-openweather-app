52024609df57d30cf95f032e368ecef5d89f7dfa
Alexandre Bazbazian <alexandre.bazbazian@free.fr>
2015-03-28 15:49:26 +0100

    Refonte des couleurs

--- app/js/filters.js
+++ app/js/filters.js
@@ -17,30 +17,32 @@ angular.module('openWeatherApp.filters', [])
   }])
 
   // Colortion du header en fonction de la temperature
-  // Glacial : [<-5]
-  // Tres froid : bleu clair : [-5;0]
+  // Glacial : bleu clair [<-5]
+  // Tres froid : bleu : [-5;0]
   // Froid : vert : [0;10]
   // Frais : jaune : [10;15]
   // Doux : orange [15;20]
-  // Chaud : rouge : [20;30]
-  // Tres chaud : rouge foncé : [30;35]
-  // Canicule : [>35]
+  // Chaud : orange fonce : [20;30]
+  // Tres chaud : rouge : [30;35]
+  // Canicule : cramoisi [>35]
   .filter('colorTemp', [function() {
     return function (temp) {
         if (temp < -5) {
-          return "bleu clair";
+          return "bleu-clair";
         } else if (temp > -5 && temp <= 0){
-          return "vert";
+          return "bleu";
         } else if (temp > 0  && temp <= 10){
+          return "vert";
+        }  else if (temp > 10 && temp <= 15){
           return "jaune";
         } else if (temp > 10 && temp <= 20){
           return "orange";
         } else if (temp > 20 && temp <= 30){
-          return "orange fonce";
+          return "orange-fonce";
         } else if (temp > 30 && temp <= 35){
           return "rouge ";
         } else if (temp > 35){
-          return "rouge fonce";
+          return "cramoisi";
         }
     }
   }])
